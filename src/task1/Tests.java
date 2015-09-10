1a6e285bd041f4cb94b2bf436f964d1a5e6b5f39
Vladislav Zakharov <vladzax95@gmail.com>
2015-09-11 01:58:42 +0300

    написал тесты для первого задания

---
+++ src/task1/Tests.java
@@ -0,0 +1,70 @@
+package task1;
+
+import java.util.Random;
+
+/**
+ * Created by vlad on 11.09.15.
+ */
+public class Tests {
+
+    public static void main(String[] args){
+        makeTest(10000,1000);
+    }
+
+    public static void makeTest(int numOfIterations, int stringLenght){
+        //неохото искать коды в интернете)
+        char[] array = {'a','b','c','d','e','f','g','h','i','g','k','l','m','n','o','p','7','1','0','3'};
+        for (int i = 0; i < numOfIterations; i++) {
+            String string = "";
+            for (int j = 0; j < stringLenght; j++)
+                string += array[ getRandomInt(0, array.length-1) ];
+
+            int result = Start.process(string);
+
+            if (result == -1){
+                if (!checkOnRepeats(string)){
+                    System.out.println("Test failed");
+                    return;
+                }
+            } else {
+                if (!checkLonely(string,result)){
+                    System.out.println("Test failed");
+                    return;
+                }
+
+                if (!checkPreviousChars(string,result)){
+                    System.out.println("Test failed");
+                    return;
+                }
+            }
+        }
+        System.out.println("Test OK");
+    }
+
+    private static int getRandomInt(int min, int max){
+        return min + (int)(Math.random() * ((max - min) + 1));
+    }
+
+    private static boolean checkOnRepeats(String str){
+        char[] charArray = str.toCharArray();
+        for (int i = 0; i < charArray.length; i++)
+            if (checkLonely(str, i)) return false;
+        return true;
+    }
+
+    private static boolean checkLonely(String str, int index){
+        char[] charArray = str.toCharArray();
+        char charForCheck = charArray[index];
+        for (char ch : charArray)
+            if (charForCheck == ch) return false;
+        return true;
+    }
+
+    private static boolean checkPreviousChars(String str, int index){
+        char[] charArray = str.toCharArray();
+        for (int i = 0; i < index; i++)
+            if (checkLonely(str, i)) return false;
+        return true;
+    }
+
+}
